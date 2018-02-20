e184d26bcaa0ed476593f75db66d2d7b7c8f218f
Andrew Emrazian <emrazian@andrews-imac.local>
2018-02-20 15:08:24 -0700

    Initial Commit

---
+++ Pokemon Browser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pokemon Browser
+//
+//  Created by Andrew Emrazian on 2/20/18.
+//  Copyright © 2018 Andrew Emrazian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pokemon Browser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pokemon Browser
+//
+//  Created by Andrew Emrazian on 2/20/18.
+//  Copyright © 2018 Andrew Emrazian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
